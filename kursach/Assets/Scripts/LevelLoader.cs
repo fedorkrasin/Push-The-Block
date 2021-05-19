e49d649639842203d489c2f366542b58fe26cbc7
fedorkrasin <kraman1501@gmail.com>
2021-05-19 15:03:17 +0300

    unity project

---
+++ kursach/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+
+    void Update()
+    {
+        // Переход к меню с объяснением
+        if (SceneManager.GetActiveScene().buildIndex == 0 && Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene(1);
+        }
+        
+        // Запуск 1 уровня из меню
+        if (SceneManager.GetActiveScene().buildIndex == 1 && Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene(2);
+        }
+        
+        // Выход из игры из главного меню
+        if (SceneManager.GetActiveScene().buildIndex == 0 && Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+        
+        // Выход в главное меню из уровней
+        if (SceneManager.GetActiveScene().buildIndex != 0 && Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(0);
+        }
+        
+        // Перезапустить уровень
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (this.CompareTag("Player") && other.CompareTag("Finish"))
+        {
+            SceneManager.LoadScene((SceneManager.GetActiveScene().buildIndex + 1) % 12);
+        }
+    }
+}
---
+++ kursach/Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private KeyCode key1;
+    [SerializeField] private KeyCode key2;
+    [SerializeField] private Vector3 moveDirecton;
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(key1))
+        {
+            GetComponent<Rigidbody>().velocity += moveDirecton;
+        }
+
+        if (Input.GetKey(key2))
+        {
+            GetComponent<Rigidbody>().velocity -= moveDirecton;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (this.CompareTag("Cube") && other.CompareTag("Cube") || this.CompareTag("Player") && other.CompareTag("Cube"))
+        {
+            foreach (Activator button in FindObjectsOfType<Activator>())
+            {
+                button.canPush = false;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (this.CompareTag("Cube") && other.CompareTag("Cube") || this.CompareTag("Player") && other.CompareTag("Cube"))
+        {
+            foreach (Activator button in FindObjectsOfType<Activator>())
+            {
+                button.canPush = true;
+            }
+        }
+    }
+}
